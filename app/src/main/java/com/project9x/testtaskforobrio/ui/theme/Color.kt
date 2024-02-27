889014cca9760b8b4b98feabe9dbdd6bceb8754f
alexeosss <oleksii.melnychenko.kpi@gmail.com>
2024-02-27 19:25:33 +0200

    init commit

--- app/src/main/java/com/project9x/testtaskforobrio/ui/theme/Color.kt
+++ app/src/main/java/com/project9x/testtaskforobrio/ui/theme/Color.kt
@@ -1,11 +1,21 @@
 package com.project9x.testtaskforobrio.ui.theme
 
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+data class Colors (
+    val backgroundColor : Color,
+    val primaryContentColor : Color,
+    val secondaryContentColor: Color
+)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val darkColorPalette = Colors(
+    backgroundColor = Color(0xFF181A21),
+    primaryContentColor = Color(0xFFEAECEF),
+    secondaryContentColor = Color(0xFFF7931A)
+)
+
+val LocalColorProvider = staticCompositionLocalOf<Colors> {
+    error("No colors")
+}
--- app/src/main/java/com/project9x/testtaskforobrio/ui/theme/Type.kt
+++ app/src/main/java/com/project9x/testtaskforobrio/ui/theme/Type.kt
@@ -1,34 +1,41 @@
 package com.project9x.testtaskforobrio.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
+data class Typography(
+    val h1: TextStyle,
+    val h2: TextStyle,
+    val h3: TextStyle,
+    val h4: TextStyle
+)
+
+val typography = Typography(
+    h1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 48.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+    ),
+    h3 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
+    ),
+    h4 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 12.sp,
     )
-    */
 )
+
+val LocalTypographyProvider = staticCompositionLocalOf<com.project9x.testtaskforobrio.ui.theme.Typography> {
+    error("No typography")
+}
